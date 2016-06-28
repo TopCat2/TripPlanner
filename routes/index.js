880cc28cfdec59e6618a2d352d14b98e13275a26
Peter Lynn <github@fantutte.net>
2016-06-28 13:54:37 -0400

    Played with lists

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,9 @@ var Restaurant = require('../models/restaurant');
 var Activity = require('../models/activity');
 
 //  HARDCODED DATA for initial testing
-var myActivities = [[], [], []];
+var myHotels = [['The Plaza'], [], []];        // First dimension is the trip day
+var myRestaurants = [['Pizza the Hutt', 'Open Market', 'The Quilted Giraffe'], [], []];
+var myActivities = [['Nap', 'Statue of Liberty', 'Bicycling'], [], []];
 var myDays = 3;
 var dayList = [];
 for (var i = 1; i <= myDays; i++) {
@@ -24,10 +26,12 @@ router.get('/', function (req, res, next){
   Promises = [Hotel.findAll(), Restaurant.findAll(), Activity.findAll()]
 
   Promise.all(Promises)
-  .spread (function(p1,p2,p3){
-    dbHotels = p1, dbRestaurants = p2, dbActivities = p3;
+  .spread(function(p1,p2,p3){
+    dbHotels = p1;
+    dbRestaurants = p2;
+    dbActivities = p3;
 res.render('index', {dbHotels:dbHotels, dbRestaurants:dbRestaurants, dbActivities:dbActivities,
-    myDays:dayList})
+    myDays:dayList, myHotels: myHotels[0], myRestaurants: myRestaurants[0], myActivities: myActivities[0]})
   })
 
 })
